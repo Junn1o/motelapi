1b1d16a34ee89450bfd47b9004cc93d6d30dfcdf
Junnio <30598484+Junn1o@users.noreply.github.com>
2023-09-16 23:15:55 +0700

    Error need resolve

---
+++ motel/motel/Models/DTO/CategoryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace motel.Models.DTO
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ motel/motel/Models/DTO/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace motel.Models.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public bool gender { get; set; }
+        public string address { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+        public string rolename { get; set; }
+        public string tier { get; set; }
+        public string? actualFile { get; set; }
+        public string FormattedBirthday { get; set; }
+        public string FormattedDatecreated { get; set; }
+    }
+}
