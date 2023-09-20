0eba63bd830b3609bebb42b0bc349039bf48bdf3
phu <phu3365@gmail,com>
2023-09-20 13:37:45 +0700

    chua pull program

--- motel/motel/Program.cs
+++ motel/motel/Program.cs
@@ -1,7 +1,13 @@
 using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using motel.Data;
 using motel.Repositories;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +19,51 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<AdminDbContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("AdminDbContext")));
 builder.Services.AddScoped<IUserRepositories, UserRepositories>();
 builder.Services.AddScoped<ITierRepositories, TierRepositories>();
 builder.Services.AddScoped<IRoleRepositories, RoleRepositories>();
-var app = builder.Build();
+builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option =>
+ option.TokenValidationParameters = new TokenValidationParameters
+ {
+     ValidateIssuer = true,
+     ValidateAudience = true,
+     ValidateLifetime = true,
+     ValidateIssuerSigningKey = true,
+     ValidIssuer = builder.Configuration["Jwt:Issuer"],
+     ValidAudience = builder.Configuration["Jwt:Audience"],
+     ClockSkew = TimeSpan.Zero,
+     IssuerSigningKey = new SymmetricSecurityKey(
+ Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+ });
+builder.Services.AddIdentityCore<IdentityUser>()
+ .AddRoles<IdentityRole>()
+ .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("Admin")
+ .AddEntityFrameworkStores<AdminDbContext>()
+ .AddDefaultTokenProviders();
+builder.Services.Configure<IdentityOptions>(option =>
+{
+    option.Password.RequireDigit = false;// Yêu c?u v? password ch?a ký s?: không
+    option.Password.RequireLowercase = false;
+    option.Password.RequireNonAlphanumeric = false;
+    option.Password.RequireUppercase = false;
+    option.Password.RequiredLength = 6;
+    option.Password.RequiredUniqueChars = 1;
+});
 
-// Configure the HTTP request pipeline.
+IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile("appsettings.json")
+            .Build();
+builder.Services.Configure<JwtSecurityToken>(configuration.GetSection("JwtSettings"));
+
+var app = builder.Build();
+app.UseAuthorization();
+app.UseAuthentication();
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
