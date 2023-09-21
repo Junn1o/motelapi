05c5cb1e8fc1fc8e9e8d9f327c733b26b548dc75
Junnio <nphan218@gmail.com>
2023-09-21 08:25:24 +0700

    ok

--- motel/motel/Program.cs
+++ motel/motel/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ITierRepositories, TierRepositories>();
 builder.Services.AddScoped<IRoleRepositories, RoleRepositories>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option =>
- option.TokenValidationParameters = new TokenValidationParameters
+option.TokenValidationParameters = new TokenValidationParameters
  {
      ValidateIssuer = true,
      ValidateAudience = true,
@@ -35,12 +35,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
      IssuerSigningKey = new SymmetricSecurityKey(
  Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
  });
-builder.Services.Configure<FormOptions>(o =>
-{
-    o.ValueLengthLimit = int.MaxValue;
-    o.MultipartBodyLengthLimit = int.MaxValue;
-    o.MemoryBufferThreshold = int.MaxValue;
-});
 var app = builder.Build();
 app.UseAuthentication();
 // Configure the HTTP request pipeline.
